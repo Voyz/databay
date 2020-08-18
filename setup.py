025b36fd504e545907707359edcb6fc51f73a9dc
voyz <voy1982@yahoo.co.uk>
2020-08-18 18:41:08 +0700

    added v0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name = 'databay',
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist"]),
-    version = '0.1.0-alpha.5',
+    version = '0.1.2',
     license='Apache-2.0',
     description = 'Databay is a Python interface for scheduled data transfer. It facilitates transfer of (any) data from A to B, on a scheduled interval.',
     long_description=long_description,
@@ -16,7 +16,7 @@ setup(
     author = 'Voy Zan',
     author_email = 'voy1982@yahoo.co.uk',
     url = 'https://github.com/Voyz/databay',
-    download_url = 'https://github.com/Voyz/databay/archive/v0.1.0-alpha.5.tar.gz',
+    download_url = 'https://github.com/Voyz/databay/archive/v0.1.2.tar.gz',
     keywords = ['data transfer', 'data production', 'data consumption', 'schedule', 'scheduled data transfer', 'scheduled transfer', 'data flow', 'repeated data transfer'],
     install_requires=[
         'APScheduler<4.0.0',
@@ -27,9 +27,9 @@ setup(
         "MongoOutlet": ["pymongo>=3.10.1"],
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'TOPIC :: SOFTWARE DEVELOPMENT :: LIBRARIES :: PYTHON MODULES',
         'Programming Language :: Python :: 3.7',
     ],
 )
