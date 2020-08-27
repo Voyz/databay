68020b890d39a76c2946553769c8f6ee6f864e86
voyz <voy1982@yahoo.co.uk>
2020-08-27 16:41:41 +0700

    released v0.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name = 'databay',
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist"]),
-    version = '0.1.4',
+    version = '0.1.5',
     license='Apache-2.0',
     description = 'Databay is a Python interface for scheduled data transfer. It facilitates transfer of (any) data from A to B, on a scheduled interval.',
     long_description=long_description,
@@ -16,7 +16,7 @@ setup(
     author = 'Voy Zan',
     author_email = 'voy1982@yahoo.co.uk',
     url = 'https://github.com/Voyz/databay',
-    download_url = 'https://github.com/Voyz/databay/archive/v0.1.4.tar.gz',
+    download_url = 'https://github.com/Voyz/databay/archive/v0.1.5.tar.gz',
     keywords = ['data transfer', 'data production', 'data consumption', 'schedule', 'scheduled data transfer', 'scheduled transfer', 'data flow', 'repeated data transfer'],
     install_requires=[
         'APScheduler<4.0.0',
@@ -25,6 +25,7 @@ setup(
     extras_require={
         "HttpInlet": ["aiohttp>=3.6.2"],
         "MongoOutlet": ["pymongo>=3.10.1"],
+        "all": ["aiohttp>=3.6.2", "pymongo>=3.10.1"]
     },
     classifiers=[
         'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
