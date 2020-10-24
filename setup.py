9fbe75768f84a044fb92890289e1fac09046d703
Charles Pierse <charlespierse@gmail.com>
2020-10-24 14:31:18 +0100

    Add python 3.8 to classifiers param

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     python_requires='>=3.7 <=3.8'
 )
