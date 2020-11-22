48b557b956fee8066b25024c12db3b99e97e0453
Charles Pierse <charlespierse@gmail.com>
2020-11-22 16:35:33 +0000

    Combine twitter timeline inlet and user inlet

--- examples/twitter_inlet.py
+++ examples/twitter_inlet.py
@@ -7,34 +7,8 @@ from databay.planners import ApsPlanner, SchedulePlanner
 from tweepy.models import Status
 
 
-class TwitterTimelineInlet(Inlet):
-
-    def __init__(self, api: tweepy.API, most_recent_id=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.api = api
-
-        # this will ensure we only every pull tweets that haven't been handled
-        self.most_recent_id = most_recent_id
-
-    def pull(self, update):
-        if self.most_recent_id is not None:
-            public_tweets = self.api.home_timeline(
-                since_id=self.most_recent_id)
-        else:
-            public_tweets = self.api.home_timeline()
-
-        if len(public_tweets) > 0:
-            # 0th tweet is most recent
-            self.most_recent_id = public_tweets[0].id
-
-        tweets = []
-        for tweet in public_tweets:
-            tweets.append({"user": tweet.user.screen_name, "text": tweet.text})
-        return tweets
-
-
-class TwitterUserInlet(Inlet):
-    def __init__(self, api: tweepy.API, user: str, most_recent_id=None, *args, **kwargs):
+class TwitterInlet(Inlet):
+    def __init__(self, api: tweepy.API, user: str = None, most_recent_id=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.api = api
         self.user = user
@@ -42,22 +16,34 @@ class TwitterUserInlet(Inlet):
         # this will ensure we only every pull tweets that haven't been handled
         self.most_recent_id = most_recent_id
 
-    def pull(self, update):
-        if self.most_recent_id is not None:
-            public_tweets = self.api.user_timeline(
-                self.user, since_id=self.most_recent_id)
+        if self.user is None:
+            self.is_user_timeline = False
         else:
-            public_tweets = self.api.user_timeline(
-                self.user)
+            self.is_user_timeline = True
+
+    def pull(self, update):
+        if self.is_user_timeline:
+            if self.most_recent_id is not None:
+                public_tweets = self.api.user_timeline(
+                    self.user, since_id=self.most_recent_id)
+            else:
+                public_tweets = self.api.user_timeline(
+                    self.user)
+        else:
+            if self.most_recent_id is not None:
+                public_tweets = self.api.home_timeline(
+                    since_id=self.most_recent_id)
+            else:
+                public_tweets = self.api.home_timeline()
+
         if len(public_tweets) > 0:
             # 0th tweet is most recent
             self.most_recent_id = public_tweets[0].id
+
         tweets = []
         for tweet in public_tweets:
             tweets.append({"user": tweet.user.screen_name, "text": tweet.text})
-
         return tweets
-
 
 # gets twitter api secrets and keys from environment vars
 consumer_key = os.getenv("twitter_key")
@@ -76,7 +62,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
 # create TwitterUserInlet() pointed at a specific account name
-twitter_user_inlet = TwitterUserInlet(api, "@BarackObama")
+twitter_user_inlet = TwitterInlet(api)
 
 link = Link(twitter_user_inlet, PrintOutlet(only_payload=True),
             interval=30, tags='twitter_user_timeline')
