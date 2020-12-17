415f13d0d2e1b2b1076fd642af3067b7192e5766
voyz <voy1982@yahoo.co.uk>
2020-12-17 19:16:14 +0700

    updated setup for 0.2.0

--- setup.py
+++ setup.py
@@ -2,24 +2,23 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='databay',
     packages=find_packages(exclude=[
-                           "*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist"]),
-    version='0.1.6',
+        "*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist"]),
+    version='0.2.0',
     license='Apache-2.0',
     description='Databay is a Python interface for scheduled data transfer. It facilitates transfer of (any) data from A to B, on a scheduled interval.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='Voy Zan',
+    author='Voy Zan, Charles Pierse',
     author_email='voy1982@yahoo.co.uk',
     url='https://github.com/Voyz/databay',
-    # download_url = 'https://github.com/Voyz/databay/archive/v0.1.5.tar.gz',
     keywords=['data transfer', 'data production', 'data consumption', 'schedule',
-              'scheduled data transfer', 'scheduled transfer', 'data flow', 'repeated data transfer'],
+                'scheduled data transfer', 'scheduled transfer', 'data flow', 'repeated data transfer'],
     install_requires=[
         'APScheduler<4.0.0',
         'schedule<1.0.0'
@@ -30,11 +29,12 @@ setup(
         "all": ["aiohttp>=3.6.2", "pymongo>=3.10.1"]
     },
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Development Status :: 4 - Beta',
+        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ]
 )
